61da5dad6d695a0bd828f27ce944712e4c41c2cb
me14nie <mel.trelet@gmail.com>
2025-06-11 14:38:13 +0200

    test

--- libraries/libraries/fct.py
+++ libraries/libraries/fct.py
@@ -1,6 +1,24 @@
 import pandas as pd
+from difflib import SequenceMatcher
 
-def process(value):
+class ColumnProcessor:
+    def process(self, value):
+        raise NotImplementedError("Subclasses should implement this!")
+
+    def compare(self, val1, val2):
+        raise NotImplementedError("Subclasses should implement this!")
+
+
+class StringColumnProcessor(ColumnProcessor):
+    def process(self, value):
         if pd.isna(value) or value == "N/A":
             return ""
         return str(value).replace(" ", "").replace(".", "").replace(",", "").lower()
+
+    def compare(self, val1, val2):
+        if val1 == "" and val2 != "":
+            return "False Positive"
+        elif val1 != "" and val2 == "":
+            return "False Negative"
+        similarity = SequenceMatcher(None, val1, val2).ratio()
+        return "Correct" if similarity == 1 else "Incorrect"
