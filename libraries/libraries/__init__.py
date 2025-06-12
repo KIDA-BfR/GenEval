b6992f3f2bf84f4e94e889fbac35cbb3ed120413
me14nie <mel.trelet@gmail.com>
2025-06-12 13:59:35 +0200

    test

--- libraries/libraries/__init__.py
+++ libraries/libraries/__init__.py
@@ -1,2 +1,6 @@
 from .valid_process_class import ColumnProcessor, StringColumnProcessor, StringColumnNoisyProcessor, ListColumnProcessor
 from .valid_process_class import BooleanListColumnProcessor, KeywordListColumnProcessor, OrderedListColumnProcessor, NumericColumnProcessor
+
+__all__ = ['StringColumnProcessor', 'StringColumnNoisyProcessor', 'ListColumnProcessor',
+           'BooleanListColumnProcessor', 'KeywordListColumnProcessor',
+           'OrderedListColumnProcessor', 'NumericColumnProcessor']
