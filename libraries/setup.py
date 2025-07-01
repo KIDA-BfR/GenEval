cdfd377f0e4bf7273bb978f3c6c12db3319b2e4e
me14nie <mel.trelet@gmail.com>
2025-07-01 10:17:22 +0200

    setup modif

--- libraries/setup.py
+++ libraries/setup.py
@@ -4,7 +4,17 @@ setup(
     name="libraries",
     version="0.1",
     packages=find_packages(),
-    author_email="Melanie.Trelet@bfr.bund.de",
-    url="https://github.com/KIDA-BfR/GenEval.git"
+    description= "a library helping you checking if your data extraction is valid, by comparing it to a ground truth file.",
+    long_description= open('README.md').read(),
+    long_description_content_type= "text/markdown",
+    author= "",
+    author_email="",
+    url="https://github.com/KIDA-BfR/GenEval.git",
     #don't know if it's the right access path
+    classifiers= [
+        "Programming Language:: ...",
+        "License:: ...",
+        "Operating System :: ...",
+    ],
+    python_requires= ">= ...",
 )
