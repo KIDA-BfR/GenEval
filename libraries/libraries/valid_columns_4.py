195e72230fc489ca06223183c05383842cc2ed11
me14nie <mel.trelet@gmail.com>
2025-07-01 12:26:34 +0200

    up 1 and 4

--- libraries/libraries/valid_columns_4.py
+++ libraries/libraries/valid_columns_4.py
@@ -1,40 +1,86 @@
+import json
+import pandas as pd
+from collections import defaultdict
 from libraries.valid_class_3 import NumericColumnProcessor, StringColumnProcessor, StringNoisyColumnProcessor, List_of_Numbers_ColumnProcessor, Ordered_List_of_Numbers_ColumnProcessor, List_of_Strings_ColumnProcessor
 
-def define_columns(table_truth):
-    
-    #defining how many columns
+# Have the choice of 2 functions 
+# The first one is better-suited for smaller tables
+# You input a .txt file (the attribution argument) in which you already made the choice of which processing method 
+# for each column by writing a list. The list is only the abbreviation of the processing method, in the same order
+# as the columns go, and coma-separated.
+# Time-consuming but best if you already know you don't want the default values.
+
+# The second one is better-suited for bigger tables
+# The code automatically choose a processing method for each column, depending on the type of content inside
+# There are default choices, but you can modify it and choose a more suitable processing method via the JSON file 
+# that you get in the end. Run it in the next function to apply the modifications, after manually editing the JSON file.
+
+def manual_process_for_columns_attribution(table_truth, attribution):
+    # Defining how many columns
     table_truth.columns = table_truth.columns.str.strip()
     nb_columns = len(table_truth.columns)
 
-    #choosing processing method for each column
-    liste_attribution = []
-    for i in range (nb_columns):
-        print("What type of processing ?")
-        print("Choose from this table: ")
-        print("Processing methods ")
-        print("-"*19)
-        print("1 - NumericColumnProcessor()")
-        print("2 - StringColumnProcessor()")
-        print("3 - StringNoisyColumnProcessor()")
-        print("4 - List_of_Numbers_ColumnProcessor()")
-        print("5 - Ordered_List_of_Numbers_ColumnProcessor()")
-        print("6 - List_of_Strings_ColumnProcessor()")
-        c = int(input("Choose a number between 1 to 6"))
-        if c == 1:
-            liste_attribution.append(NumericColumnProcessor())
-        elif c == 2:
-            liste_attribution.append(StringColumnProcessor())
-        elif c == 3:
-            liste_attribution.append(StringNoisyColumnProcessor())
-        elif c == 4:
-            liste_attribution.append(List_of_Numbers_ColumnProcessor())
-        elif c == 5:
-            liste_attribution.append(Ordered_List_of_Numbers_ColumnProcessor())
-        elif c == 6:
-            liste_attribution.append(List_of_Strings_ColumnProcessor())
-        else:
-            print("Invalid choice. Please select a number between 1 to 6.")
+    # Choosing the processing method for each column
+    print("What type of processing for each column ?")
+    print("Write a txt file via choosing from this table: ")
+    print("Processing methods ")
+    print("-"*19)
+    print("num - NumericColumnProcessor()")
+    print("words - StringColumnProcessor()")
+    print("words_not_exact - StringNoisyColumnProcessor()")
+    print("list_num - List_of_Numbers_ColumnProcessor()")
+    print("ordered_list_num - Ordered_List_of_Numbers_ColumnProcessor()")
+    print("list_words - List_of_Strings_ColumnProcessor()")
 
-    #pairing each columns with processing methods
-    processors = dict(zip(table_truth.columns,liste_attribution))
-    return processors 
+    
+
+
+def automatic_process_for_columns_attribution(table_truth):
+    # Sample data types and assign processing methods
+    processors = {}
+    sample_size = 5
+    columns = table_truth.columns
+    for column in columns:
+        sample = table_truth[column].dropna().sample(min(sample_size, len(table_truth[column])))
+        types_in_column = set()
+
+        for item in sample:
+            if isinstance(item, list):
+                if len(item) > 0:
+                    # Check the type of the first element in the list
+                    first_element_type = type(item[0])
+                    if all(isinstance(x, (int, float)) for x in item):
+                        types_in_column.add(list[int])
+                    elif all(isinstance(x, str) for x in item):
+                        types_in_column.add(list[str])
+                    else:
+                        types_in_column.add(list)
+                else:
+                    types_in_column.add(list)
+            else:
+                types_in_column.add(type(item))
+
+        if any(typ in (int, float) for typ in types_in_column):
+            processors[column] = NumericColumnProcessor()
+        elif any(typ is str for typ in types_in_column):
+            processors[column] = StringColumnProcessor()
+        # elif any(typ is bool for typ in types_in_column):
+        #     processors[column] = BooleanProcessor()
+        # elif any(typ is pd.Timestamp for typ in types_in_column):
+        #     processors[column] = DateTimeProcessor()
+        elif any(typ is list[int] for typ in types_in_column):
+            processors[column] = List_of_Numbers_ColumnProcessor()
+        elif any(typ is list[str] for typ in types_in_column):
+            processors[column] = List_of_Strings_ColumnProcessor()
+
+    # Save processors to a JSON file
+    processors_dict = {col: str(processor) for col, processor in processors.items()}
+    with open('attribution.json', 'w') as json_file:
+        json.dump(processors_dict, json_file, indent=4)  
+
+
+def modify_attribution(attribution):
+    global processors
+    with open('attribution.json', 'r') as json_file:
+        processors = json.load(json_file)
+    print("The attribution has been edited.") 
--- libraries/libraries/valid_import_1.py
+++ libraries/libraries/valid_import_1.py
@@ -8,10 +8,6 @@ def dataframe_impl(path_truth, path_compared):
     # Testing for CARC
     file_truth_path = path_truth
     file_compared_path = path_compared
-
-    filename_truth = Path(file_truth_path).stem
-    filename_compared = Path(file_compared_path).stem
-
     # Load files
     table_truth = pd.read_excel(file_truth_path)
     table_compared = pd.read_excel(file_compared_path)
