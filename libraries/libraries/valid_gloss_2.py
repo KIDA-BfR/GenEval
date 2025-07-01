e8581bd45c996a3a09085b2241bb0135cb35eef9
me14nie <mel.trelet@gmail.com>
2025-07-01 10:42:48 +0200

    added glossary

--- libraries/libraries/valid_gloss_2.py
+++ libraries/libraries/valid_gloss_2.py
@@ -1,8 +1,13 @@
 # Strip any leading or trailing whitespace from all column names
 # Sorting the data to normalize the comparison
+# Adding a glossary for the user to add some equivalent terms
 def sort_data(table_truth, table_compared):
     table_truth.columns = table_truth.columns.str.strip()
     table_truth = table_truth.sort_values(by=table_truth.columns[0]).reset_index(drop=True)
 
     table_compared.columns = table_compared.columns.str.strip()
     table_compared = table_compared.sort_values(by=table_compared.columns[0]).reset_index(drop=True)
+
+    glossary = {
+        
+    }
