587f5d49f728918f41e0dfcd4dd892511043166a
me14nie <mel.trelet@gmail.com>
2025-07-04 11:50:27 +0200

    added optional display for 1

--- validation_library/validation_package/load.py
+++ validation_library/validation_package/load.py
@@ -1,15 +1,19 @@
 import pandas as pd
 from pathlib import Path
+from IPython.display import display
 
 # Load the excel file containing two files to tables to compare, tables should
 # contain the same columns
 
-def reading_tables(path_truth, path_compared):
-    # Testing for CARC
-    file_truth_path = path_truth
-    file_compared_path = path_compared
+def reading_tables(path_truth, path_compared, display_tables = False):
     # Load files
-    table_truth = pd.read_excel(file_truth_path)
-    table_compared = pd.read_excel(file_compared_path)
+    table_truth = pd.read_excel(path_truth)
+    table_compared = pd.read_excel(path_compared)
+
+    # Display loaded tables if display_tables is True
+    if display_tables:
+        with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', None, 'display.max_colwidth', None):
+            display(table_truth)
+            display(table_compared)
     
-    return table_truth, table_compared
+    return "Tables are loaded"
