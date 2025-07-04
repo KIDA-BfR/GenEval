7a8417009bc1bb30e7bfd989f12af2ad33c34861
me14nie <mel.trelet@gmail.com>
2025-07-04 12:14:36 +0200

    added optional glossary

--- validation_library/validation_package/sort_and_glossary.py
+++ validation_library/validation_package/sort_and_glossary.py
@@ -1,17 +1,17 @@
 # Strip any leading or trailing whitespace from all column names
 # Sorting the data to normalize the comparison
 # Adding a glossary for the user to add some equivalent terms
-def sort_data(table_truth, table_compared):
+def sort_data(table_truth, table_compared, glossary=None):
+    # Default glossary if none provided
+    if glossary is None:
+        glossary = {}
+    
     table_truth.columns = table_truth.columns.str.strip()
     table_truth = table_truth.sort_values(by=table_truth.columns[0]).reset_index(drop=True)
 
     table_compared.columns = table_compared.columns.str.strip()
     table_compared = table_compared.sort_values(by=table_compared.columns[0]).reset_index(drop=True)
 
-    # Creating a glossary to specify if different terms have the same meaning
-    glossary = {
-
-    }
     # Applying the glossary to a specific column
     tables = [("truth", table_truth), ("compared", table_compared)]
     for name, df in tables:
@@ -30,3 +30,5 @@ def sort_data(table_truth, table_compared):
             print(f"Table {name}: The following glossary columns were not found in the DataFrame: {missing_columns}")
         if applied_columns:
             print(f"Table {name}: Glossary was applied to {applied_columns}")
+            
+    return table_truth, table_compared
