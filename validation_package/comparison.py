e944dfd95dfad761600695bac8ec88b398bc436c
me14nie <mel.trelet@gmail.com>
2025-07-31 13:01:50 +0200

    design ignore column

--- validation_package/comparison.py
+++ validation_package/comparison.py
@@ -1,6 +1,14 @@
 import pandas as pd
 
-def comparison_init(table_truth, table_compared, processors):
+def comparison_init(table_truth, table_compared, processors, ignore_columns=None):
+    if ignore_columns is None:
+        ignore_columns = []
+
+    # Filter out the columns to ignore from both tables
+    columns = [col for col in table_truth.columns if col not in ignore_columns]
+    table_truth_filtered = table_truth[columns]
+    table_compared_filtered = table_compared[columns]
+
     # Create an empty DataFrame to store the comparison results
     columns = table_truth.columns
     comparison_results = pd.DataFrame(columns=columns)
@@ -11,8 +19,8 @@ def comparison_init(table_truth, table_compared, processors):
     # Process and compare the data
     for column in columns:
         processor = processors[column]
-        processed_table1 = table_truth[column].apply(processor.process)
-        processed_table2 = table_compared[column].apply(processor.process)
+        processed_table1 = table_truth_filtered[column].apply(processor.process)
+        processed_table2 = table_compared_filtered[column].apply(processor.process)
         comparison_results[column] = [
             processor.compare(val1, val2) for val1, val2 in zip(processed_table1, processed_table2)
         ]
